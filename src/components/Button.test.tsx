dd09de1bcd6027a7b512f4e540ee20f2e2fd0715
caseycameron <mcasey.cameron@gmail.com>
2022-07-29 15:19:12 -0700

    RED - call the handler when clicked

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -2,9 +2,17 @@ import { render, screen } from '@testing-library/react'
 import user from '@testing-library/user-event';
 import Button from './Button';
 
-const countHandler = jest.fn()
+const counterHandler = jest.fn()
+afterEach(() => counterHandler.mockReset())
 
 it('should render a "button" with a count: 0', () => {
-  render(<Button count={0} incrementor={countHandler} />)
+  render(<Button count={0} incrementor={counterHandler} />)
   screen.getByText(/count: 0/i)
+})
+
+it('should render a "button" and call the handler when clicked', () => {
+  render(<Button count={0} incrementor={counterHandler} />)
+  screen.getByText(/count: 0/i)
+  user.click(screen.getByTestId(/counterbutton/i))
+  expect(counterHandler).toBeCalledTimes(0)
 })
