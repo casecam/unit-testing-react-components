8b414dc9486d0e2cdf0b9ad6a6c3494af49e1c95
caseycameron <mcasey.cameron@gmail.com>
2022-07-29 15:28:08 -0700

    RED - Render the app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders four links', () => {
+test('renders the app', () => {
   render(<App />);
+  // get Header
   screen.getByText(/\| link 1 \| link 2 \| link 3 \| link 4 \|/i)
+  // get Main --> Menu, Form, Counter
+  screen.getByText(/favorite dog/i)
+  screen.getByText(/input dog's name here:/i)
+  screen.getByRole('textbox', {
+    name: /input dog's name here:/i
+  })
+  screen.getByRole('button', {
+    name: /submit/i
+  })
+  screen.getByText(/count: 0/i)
 });
