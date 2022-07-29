0b8579c46705b77234f13c50aec71207cdb84cbb
caseycameron <mcasey.cameron@gmail.com>
2022-07-29 15:20:30 -0700

    RED - should call the handler 2x

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -10,9 +10,17 @@ it('should render a "button" with a count: 0', () => {
   screen.getByText(/count: 0/i)
 })
 
-it('should render a "button" and call the handler when clicked', () => {
+it('should call the handler when clicked once', () => {
   render(<Button count={0} incrementor={counterHandler} />)
   screen.getByText(/count: 0/i)
+  user.click(screen.getByTestId(/counterbutton/i))
+  expect(counterHandler).toBeCalledTimes(1)
+})
+
+it('should call the handler twice when clicked twice', () => {
+  render(<Button count={0} incrementor={counterHandler} />)
+  screen.getByText(/count: 0/i)
+  user.click(screen.getByTestId(/counterbutton/i))
   user.click(screen.getByTestId(/counterbutton/i))
   expect(counterHandler).toBeCalledTimes(1)
 })
